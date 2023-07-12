0868659ef9968e96ffcd589dcdb565b66af9cdad
人工智能大讲堂 <1359314830@qq.com>
2023-07-12 10:43:30 +0800

    Create lightglue.h

---
+++ lightglue.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <memory>
+#include <Windows.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/stitching.hpp"
+#include<iostream>
+#include <fstream> //for file operations
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/xfeatures2d/nonfree.hpp"
+#include "opencv2/stitching/detail/matchers.hpp"
+#include "opencv2/core/ocl.hpp"
+#include <opencv2/imgproc.hpp>
+#include "opencv2/calib3d.hpp"
+#include <onnxruntime_cxx_api.h>
+#include <math.h>
+
+using namespace cv::detail;
+using namespace cv;
+using namespace std;
+
+class LightGlue :public FeaturesMatcher
+{
+protected:
+	float m_width;
+	float m_height;
+
+	CV_WRAP_AS(apply) void operator ()(const ImageFeatures& features1, const ImageFeatures& features2,
+		CV_OUT MatchesInfo& matches_info) {
+		match(features1, features2, matches_info);
+	}
+public:
+	LightGlue(int wid, int hei);
+	void match(const ImageFeatures& features1, const ImageFeatures& features2,
+		MatchesInfo& matches_info);
+
+};
