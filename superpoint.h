82ae45db7c59236eb20cd8ac89e45fc2da67a656
人工智能大讲堂 <1359314830@qq.com>
2023-07-12 10:42:59 +0800

    Create superpoint.h

---
+++ superpoint.h
@@ -0,0 +1,41 @@
+#include <vector>
+#include <string>
+#include <memory>
+#include <Windows.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/stitching.hpp"
+#include<iostream>
+#include <fstream> //for file operations
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/xfeatures2d/nonfree.hpp"
+#include "opencv2/stitching/detail/matchers.hpp"
+#include "opencv2/core/ocl.hpp"
+#include <opencv2/imgproc.hpp>
+#include "opencv2/calib3d.hpp"
+#include <onnxruntime_cxx_api.h>
+#include <math.h>
+
+using namespace cv::detail;
+using namespace cv;
+using namespace std;
+
+class SuperPoint :public Feature2D
+{
+protected:
+	vector<float> ApplyTransform(const Mat& image, float& mean, float& std);
+public:
+	SuperPoint();
+	virtual void detectAndCompute(InputArray image, InputArray mask,
+		std::vector<KeyPoint>& keypoints,
+		OutputArray descriptors,
+		bool useProvidedKeypoints = false);
+	virtual void detect(InputArray image,
+		std::vector<KeyPoint>& keypoints,
+		InputArray mask = noArray());
+	virtual void compute(InputArray image,
+		std::vector<KeyPoint>& keypoints,
+		OutputArray descriptors);
+
+};
